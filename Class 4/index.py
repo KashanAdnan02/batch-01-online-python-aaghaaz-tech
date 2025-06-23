13e2aab225383cbccf6ec5b9e6a715f577e78a2d
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-06-23 07:01:00 -0700

    String Methods and Type Cast Str into number

---
+++ Class 4/index.py
@@ -0,0 +1,19 @@
+name = "i am KASHAN, KASHAN, KASHAN,KASHAN"
+
+# Methods
+
+# capsName = name.capitalize()
+# lowerName = name.casefold()
+# print(lowerName)
+
+# centerName = name.center(50)
+
+# print(centerName)
+
+# counting = name.count("KASHAN")
+# print(counting)
+
+
+lowerName = name.casefold()
+print(name)
+print(lowerName)
---
+++ Class 4/main.py
@@ -0,0 +1,14 @@
+# age = input("Enter your age : ")
+# print(type(age))
+# print(age - 1)
+# How to type Cast
+# int()
+
+# print(int(age) -1)
+
+# "11" # string
+# 11
+
+
+
+    
