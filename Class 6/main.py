cf5eba1640016870474df40ef6097a971b93a59b
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-07 06:34:15 -0700

    string

---
+++ Class 6/main.py
@@ -0,0 +1,42 @@
+# name = "kashan adnan"
+# newName = name.capitalize()
+# print(newName)
+
+# name = "KASHAN ADNAN"
+# newName = name.casefold()
+# print(newName)
+# Small Aega
+# Capital Aega
+
+# name = "Haris"
+# newName = name.center(20)
+# print(newName)
+
+# name ="This is the isBank"
+# countName = name.count("is")
+# print(countName)
+
+
+# name = "Hamna."
+# name.endswith("a") # true / false
+# print(name.endswith("."))
+
+# name = "K\ta\tshan"
+# print(name.expandtabs(0))
+#           012345678901
+# sentence = "haris is a student"
+# print(sentence.find("haris"))
+# # Haris(0) 1
+
+# txt = "For only {price} dollars!"
+
+# print(txt.format(price = 100))
+# print(txt.format(price = 20))
+# print(txt.format(price = 10))
+# print(txt.format(price = 49))
+
+
+name = "{name} is my name."
+print(name.format(name = "Kashan Adnan"))
+print(name.format(name = "Haris Anjum"))
+print(name.format(name = "Hamna"))
---
+++ class 7/main.py
@@ -0,0 +1,7 @@
+# print("It is working")
+
+name = "Kashan-Adnan-haris,Hamna"
+# print(name.split("-"))
+# print(name.strip())
+# print(type(name))
+# print(id(name))
