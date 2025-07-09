474500e60a88535bbb206ed8af58dc98cdac00c0
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-09 07:11:01 -0700

    if conditions

---
+++ Class 8/main.py
@@ -0,0 +1,19 @@
+
+
+# Syntax --> Likhne ka tareeka
+# if
+# Indentation
+# True
+
+
+age = int(input("Enter your age : "))
+# >=
+# ==
+# >
+# <
+# <=
+
+if age >= 18:
+    print("You can vote!")
+else:
+    print("Sorry You cannot vote!")
