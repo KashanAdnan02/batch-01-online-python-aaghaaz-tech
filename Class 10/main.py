43cf6dc6c592e509c34afe817f6006abd2305681
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-16 07:58:01 -0700

    done tuples

---
+++ Class 10/main.py
@@ -0,0 +1,20 @@
+#              0        1        2
+# students = ("Haris", "Hamna", "Hamna")
+
+# Round Brackets Refers Tuples ()
+# Accesing the Tuples
+# name = students[1]
+# print(name)
+# Negative indexing the Tuples
+# name2 = students[-3]
+# print(name2)
+
+# Tuples allow duplicate values:
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+# print(thistuple)
+
+# new_tuple = list(thistuple)
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
---
+++ Class 9/main.py
@@ -0,0 +1,56 @@
+name = "Kashan"
+email = "kashan@gmail.com"
+#               0              1     2
+# courses = ["Web Development", "Ai", "Python", "Grpahic desiging"]
+#   3
+
+
+
+print(name[2])
+# courses.append("Graphic Designing")
+## start
+## End
+# courses.clear()
+courses = ["Web dev", "Ai" , "Python","Python", True, 1212]
+# print(courses)
+# List []
+
+# courses2 = courses.copy()
+# courses2.clear()
+print(courses.count("Python"))
+# print(courses2)
+fruits = ['apple', 'banana', 'cherry']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+fruits.extend(cars)
+
+print(fruits)
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.insert(1, "orange")
+
+print(fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.pop(1)
+
+print(fruits)
+
+
+fruits = ['apple', 'banana', 'cherry','banana',]
+
+fruits.remove("banana")
+print(fruits)
+
+
+fruits.reverse()
+print(fruits)
+
+
+
+fruits.sort()
+print(fruits)
