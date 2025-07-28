adb1f13ef6553a53b2452c2c049db9941ee74b2f
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-28 07:05:08 -0700

    done

--- Class 11/main.py
+++ Class 11/main.py
@@ -49,7 +49,7 @@
 
 ## Table Generator
 
-table = int(input("Enter any number to print the table: "))
+# table = int(input("Enter any number to print the table: "))
 # 4 X 1 = 4
 # 4 X 2 = 8
 # 4 X 3 = 12
@@ -57,15 +57,26 @@ table = int(input("Enter any number to print the table: "))
 # print(table * 2)
 # print(table * 3)
 # print(table * 4)
-print(f"{table} X 1 = {table*1}")
-print(f"{table} X 2 = {table*2}")
-print(f"{table} X 3 = {table*3}")
-print(f"{table} X 4 = {table*4}")
-print(f"{table} X 5 = {table*5}")
-print(f"{table} X 6 = {table*6}")
-print(f"{table} X 7 = {table*7}")
-print(f"{table} X 8 = {table*8}")
-print(f"{table} X 9 = {table*9}")
-print(f"{table} X 10 = {table*10}")
+# print(f"{table} X 1 = {table*1}")
+# print(f"{table} X 2 = {table*2}")
+# print(f"{table} X 3 = {table*3}")
+# print(f"{table} X 4 = {table*4}")
+# print(f"{table} X 5 = {table*5}")
+# print(f"{table} X 6 = {table*6}")
+# print(f"{table} X 7 = {table*7}")
+# print(f"{table} X 8 = {table*8}")
+# print(f"{table} X 9 = {table*9}")
+# print(f"{table} X 10 = {table*10}")
 
 # HINT : Range
+# ["Kashan",]
+
+# dictionary =  {
+#     "name" : "Kashan"
+# } 
+
+
+# Function 
+#  Exponent
+a = 9 ** 3
+print(a)
