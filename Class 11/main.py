6cb34fa530335a61fdd2563bbcab44f9b93a69fb
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-28 06:55:44 -0700

    done for loop

---
+++ Class 11/main.py
@@ -0,0 +1,71 @@
+# fruits = ["Apple", "Banana"]
+# for name in fruits:
+#     print(name.upper())
+
+# Array || Javascript
+# List || Python
+
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+
+# 100
+
+# conditions
+
+# num = range(5)
+# print(num)
+# for i in range(5):
+#     print("Hello") 
+
+# for a in "kashan":
+#     print(a)
+
+# 1st Way
+# kashan
+
+# 2nd Way
+# k
+# a
+# s
+# h
+# a
+# n
+
+
+# students = ["Haris", "Hamna", "Kashan"]
+# list ["Haris", "Hamna", "Kashan"]
+# range range(5)
+# string "kashan" 
+
+# for a in students:
+#     print(a)
+#     if a == "Kashan":
+#         break
+
+## Table Generator
+
+table = int(input("Enter any number to print the table: "))
+# 4 X 1 = 4
+# 4 X 2 = 8
+# 4 X 3 = 12
+# print(table * 1)
+# print(table * 2)
+# print(table * 3)
+# print(table * 4)
+print(f"{table} X 1 = {table*1}")
+print(f"{table} X 2 = {table*2}")
+print(f"{table} X 3 = {table*3}")
+print(f"{table} X 4 = {table*4}")
+print(f"{table} X 5 = {table*5}")
+print(f"{table} X 6 = {table*6}")
+print(f"{table} X 7 = {table*7}")
+print(f"{table} X 8 = {table*8}")
+print(f"{table} X 9 = {table*9}")
+print(f"{table} X 10 = {table*10}")
+
+# HINT : Range
