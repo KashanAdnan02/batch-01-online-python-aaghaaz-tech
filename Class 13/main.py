3106b83ee67be9242f7726a3fedf9a1fc5c906a2
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-08-13 09:41:25 -0700

    done

---
+++ Class 13/main.py
@@ -0,0 +1,23 @@
+# def sumtwonumbers():
+#     # andar ka code chala
+#     num1 = int(input("Enter your 1st num: "))
+#     num2 = int(input("Enter your 2nd num: "))
+#     sum = num1 + num2
+#     print(sum)
+#     return num1 * num2
+
+# # NONE
+# ## call kya 
+# variable = sumtwonumbers()
+# print(variable)
+# # Next Level  Return()
+
+
+
+
+def hello():
+    return "Hello Kashan"
+
+text = hello()
+
+print(text)
